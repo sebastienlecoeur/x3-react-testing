a361a2e56c820978a130f2c5154fcaf8ea26432e
Sebastien <sebastien.lecoeur@sage.com>
2017-07-12 14:29:34 +0200

    Commented Hello component and Added Sage logo

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-// import App from './App';
-import Hello from './components/Hello';
+import App from './App';
+// import Hello from './components/Hello';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('root') as HTMLElement
-// );
-
 ReactDOM.render(
-  <Hello name="Sébastien" enthusiasmLevel={3} />,
+  <App />,
   document.getElementById('root') as HTMLElement
 );
+
+// ReactDOM.render(
+//   <Hello name="Sébastien" enthusiasmLevel={3} />,
+//   document.getElementById('root') as HTMLElement
+// );
 
 registerServiceWorker();
