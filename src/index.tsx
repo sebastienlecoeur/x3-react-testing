b375f7d4f6ca6ad53ee8b1eddd76d1176597485e
Sebastien <sebastien.lecoeur@sage.com>
2017-07-12 14:06:53 +0200

    Added Hello component inside /components folder and in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
+import Hello from './components/Hello';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root') as HTMLElement
+// );
+
 ReactDOM.render(
-  <App />,
+  <Hello name="Sébastien" enthusiasmLevel={3} />,
   document.getElementById('root') as HTMLElement
 );
+
 registerServiceWorker();
